0f3d2d0fc639d02b97c1b86ecd4433c171c634f9
Garashia <117160460+Garashia@users.noreply.github.com>
2023-03-07 12:33:58 +0900

    コルーチンを使ったサイズ変更

--- clone/Assets/script/Character.cs
+++ clone/Assets/script/Character.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    float speed = 5.0f;
-    float jump = 0.0f;
+    [SerializeField]
+    private float speed = 5.0f;
+
+    [SerializeField]
+    private float jumpPower = 8.0f;
+
+    private float jump = 0.0f;
 
     public GameObject Water;
+
+    private float size = 0;
 
     // Update is called once per frame
     void Update()
@@ -26,7 +33,7 @@ public class Character : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            jump = 8.0f;
+            jump = jumpPower;
         }
 
         transform.position += jump * transform.up * Time.deltaTime;
@@ -51,7 +58,18 @@ public class Character : MonoBehaviour
     {
         if (coll.gameObject.name == "waterCollider")
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            StartCoroutine(ReplayerSize());
+            transform.localScale = new Vector3(size, size, 1);
+        }
+    }
+
+    IEnumerator ReplayerSize()
+    {
+        size = transform.localScale.x;
+        while(size <= 1)
+        {
+            size += 0.01f;
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
