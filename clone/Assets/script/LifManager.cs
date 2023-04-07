18ccffa14679952806f3f705de7adf52f29f9ac2
TSITCL\it222172 <it222172@trident.ac.jp>
2023-04-07 12:35:27 +0900

    ギミックの制作

--- clone/Assets/script/LifManager.cs
+++ clone/Assets/script/LifManager.cs
@@ -24,10 +24,33 @@ public class LifManager : MonoBehaviour
     {
         lift.transform.position += new Vector3(plusX, 0.0f,  0.0f);
 
+        if (lift.transform.position.x >= fastPos.x)
+        {
+            RIGHT = true;
+            LEFT = false;
+            plusX = 0.0f;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.name == "stage")
+        {
+            plusX = 0.0f;
+            RIGHT = true;
+            LEFT = false;
+        }
+
+        if ((collision.gameObject.name == "bone_7" || collision.gameObject.name == "bone_8"
+            || collision.gameObject.name == "bone_9" || collision.gameObject.name == "bone_10") && lift.transform.position.x >= fastPos.x && LEFT == true)
+        {
+            plusX = -0.01f;
+        }
+        if ((collision.gameObject.name == "bone_7" || collision.gameObject.name == "bone_8"
+    || collision.gameObject.name == "bone_9" || collision.gameObject.name == "bone_10") && lift.transform.position.x <= fastPos.x && RIGHT == true)
+        {
+            plusX = 0.01f;
+        }
 
     }
 }
