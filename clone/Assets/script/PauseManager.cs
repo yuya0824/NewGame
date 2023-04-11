f98d1b89972e33827a41cd5abea26b026301af93
TSITCL\it222172 <it222172@trident.ac.jp>
2023-04-11 13:54:30 +0900

    Pause画面の作成

--- clone/Assets/script/PauseManager.cs
+++ clone/Assets/script/PauseManager.cs
@@ -6,6 +6,8 @@ public class PauseManager : MonoBehaviour
 {
     public GameObject pause_1, pause_2, pause_3;
     private Vector2 pos_1, pos_2, pos_3, SelectPos;
+    private bool pau1, pau2, pau3;
+    private int pauseSelect;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +17,76 @@ public class PauseManager : MonoBehaviour
         pos_3 = pause_3.transform.position;
 
         SelectPos = pos_1;
+        pau1 = true;
+        pau2 = false;
+        pau3 = false;
+        pauseSelect = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MOVESELECT();
+        if (Input.GetKeyDown(KeyCode.DownArrow)) pauseSelect++;
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) pauseSelect--;
+        if (pauseSelect > 2) pauseSelect = 0;
+        else if (pauseSelect < 0) pauseSelect = 2;
+    }
+
+    void MOVESELECT()
+    {
+        if (pauseSelect == 0)
+        {
+            SelectPos = pos_1;
+            //if (Input.GetKey(KeyCode.DownArrow))
+            //{
+            //    SelectPos = pos_2;
+            //    pau1 = false;
+            //    pau2 = true;
+            //}
+            //else if(Input.GetKey(KeyCode.UpArrow))
+            //{
+            //    SelectPos = pos_3;
+            //    pau1 = false;
+            //    pau3 = true;
+            //}
+        }
+
+        else if (pauseSelect == 1)
+        {
+            SelectPos = pos_2;
+
+            //if (Input.GetKeyDown(KeyCode.DownArrow))
+            //{
+            //    SelectPos = pos_3;
+            //    pau2 = false;
+            //    pau3 = true;
+            //}
+            //else if (Input.GetKeyDown(KeyCode.UpArrow))
+            //{
+            //    SelectPos = pos_1;
+            //    pau2 = false;
+            //    pau1 = true;
+            //}
+        }
+
+        else if (pauseSelect == 2)
+        {
+            SelectPos = pos_3;
+            //if (Input.GetKeyDown(KeyCode.DownArrow))
+            //{
+            //    SelectPos = pos_1;
+            //    pau3 = false;
+            //    pau1 = true;
+            //}
+            //else if (Input.GetKeyDown(KeyCode.UpArrow))
+            //{
+            //    SelectPos = pos_2;
+            //    pau3 = false;
+            //    pau2 = true;
+            //}
+        }
+
+        transform.position = SelectPos;
     }
 }
