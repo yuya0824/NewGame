c17dbf6ac82cc3178d6dd0897bd8803c44375297
Garashia <117160460+Garashia@users.noreply.github.com>
2023-04-11 10:42:53 +0900

    TimeManager2

---
+++ clone/Assets/TimeManager2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager2 : MonoBehaviour
+{
+    private float timer;
+    [SerializeField]
+    private Text textTime;
+    private bool oneOrThen;
+    // Start is called before the first frame update
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            oneOrThen = !oneOrThen;
+        }
+
+        if (oneOrThen)
+            textTime.text = string.Format("{0:00.00} •b", Time.time - timer);
+        else
+    }
+}
